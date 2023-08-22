97537994a5799ed6e0da63f0ae78a2aad16b3b6b
ferchox920 <fernandoramones92@gmail.com>
2023-08-22 18:06:05 -0300

    firts commit

--- main.go
+++ main.go
@@ -4,19 +4,31 @@ import (
 	"log"
 
 	"github.com/ferchox920/fiber-api/database"
+	"github.com/ferchox920/fiber-api/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
-func welcome(c *fiber.Ctx) error{
+func welcome(c *fiber.Ctx) error {
 	return c.SendString("Welcome to my API")
+}
+
+func setupRoutes(app *fiber.App) {
+	// Welcome endpoint
+	app.Get("/api", welcome)
+	app.Post("/api/users", routes.CreateUser)
+	app.Get("/api/users/:id", routes.FindUserByID)
+	app.Get("/api/users", routes.FindAllUsers)
+	app.Put("/api/users/:id", routes.UpdateUser)
+	app.Delete("/api/users/:id", routes.DeleteUser)
+
 }
 
 func main() {
 
 	database.ConnectDb()
-	app:= fiber.New()
+	app := fiber.New()
 
-	app.Get("/api",welcome)
+	setupRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
 }
---
+++ routes/user.go
@@ -0,0 +1,88 @@
+package routes
+
+import (
+	"github.com/ferchox920/fiber-api/database"
+	"github.com/ferchox920/fiber-api/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+type User struct {
+	// This is not the model, more like a serializer
+	ID        uint   `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+func CreateResponseUser(user models.User) User {
+	return User{ID: user.ID, FirstName: user.FirstName, LastName: user.LastName}
+}
+
+func CreateUser(c *fiber.Ctx) error {
+	var user models.User
+
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	database.Database.Db.Create(&user)
+	responseUser := CreateResponseUser(user)
+	return c.Status(200).JSON(responseUser)
+}
+
+func FindUserByID(c *fiber.Ctx) error {
+	userID := c.Params("id") // Obtener el ID de los parámetros de la URL
+
+	var user models.User
+	result := database.Database.Db.First(&user, userID)
+
+	if result.Error != nil {
+		return c.Status(404).JSON(fiber.Map{"message": "Usuario no encontrado"})
+	}
+
+	responseUser := CreateResponseUser(user)
+	return c.Status(200).JSON(responseUser)
+}
+
+func UpdateUser(c *fiber.Ctx) error {
+	userID := c.Params("id") // Obtener el ID de los parámetros de la URL
+
+	var user models.User
+	result := database.Database.Db.First(&user, userID)
+
+	if result.Error != nil {
+		return c.Status(404).JSON(fiber.Map{"message": "Usuario no encontrado"})
+	}
+
+	var updateUser models.User
+	if err := c.BodyParser(&updateUser); err != nil {
+		return c.Status(400).JSON(fiber.Map{"message": err.Error()})
+	}
+
+	database.Database.Db.Model(&user).Updates(updateUser)
+	responseUser := CreateResponseUser(user)
+	return c.Status(200).JSON(responseUser)
+}
+func DeleteUser(c *fiber.Ctx) error {
+	userID := c.Params("id") // Obtener el ID de los parámetros de la URL
+
+	var user models.User
+	result := database.Database.Db.First(&user, userID)
+
+	if result.Error != nil {
+		return c.Status(404).JSON(fiber.Map{"message": "Usuario no encontrado"})
+	}
+
+	database.Database.Db.Delete(&user)
+	return c.Status(204).JSON(nil) // 204 significa que no hay contenido en la respuesta
+}
+func FindAllUsers(c *fiber.Ctx) error {
+	var users []models.User
+	database.Database.Db.Find(&users)
+
+	responseUsers := make([]User, len(users))
+	for i, user := range users {
+		responseUsers[i] = CreateResponseUser(user)
+	}
+
+	return c.Status(200).JSON(responseUsers)
+}
